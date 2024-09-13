3fdf9ed203616488542003caad5e32fe2e5f9096
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-09-13 15:54:06 +0300

    Register page stylization

--- thenovelclub/src/components/Register/Register.js
+++ thenovelclub/src/components/Register/Register.js
@@ -1,7 +1,34 @@
-
+import { Link } from "react-router-dom";
+import './Register.css';
 
 export const Register = () => {
     return (
-        <p>Register page</p>
+        <>
+        <div className="background">
+            <div className="shape"></div>
+            <div className="shape"></div>
+        </div>
+        <form className="register-form">
+            <h3>Register</h3>
+
+            <label htmlFor="email">Email</label>
+            <input type="text" placeholder="Email" id="email" />
+
+            <label htmlFor="username">Username</label>
+            <input type="text" placeholder="Username" id="username" />
+
+            <label htmlFor="password">Password</label>
+            <input type="password" placeholder="Password" id="password" />
+
+            <label htmlFor="confirm-password">Confirm Password</label>
+            <input type="password" placeholder="Confirm Password" id="confirm-password" />
+
+            <button>Register</button>
+            <div className="re-route">
+                <p>You already have an account... please go to the <Link to="/client-login">Login page</Link></p>
+            </div>
+        </form>
+        </>
+
     )
 }
