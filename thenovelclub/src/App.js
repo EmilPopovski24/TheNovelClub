d5ee0a83d81f336a5d491b73dcfa82f26243ca68
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-09-13 14:01:20 +0300

    update

--- thenovelclub/src/App.js
+++ thenovelclub/src/App.js
@@ -1,15 +1,15 @@
 import { Route, Routes } from 'react-router-dom';
-import './App.css';
 import { Header } from './components/Header/Header';
 import { Home } from './components/Home/Home';
 import { Footer } from './components/Footer/Footer';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Routes>
-          <Route path='/home' element={<Home />} />
+          <Route path='/' element={<Home />} />
       </Routes>
       < Footer />
     </div>
