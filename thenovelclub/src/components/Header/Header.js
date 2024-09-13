b56904d566423fdb3809a37ba3ba2ca84fecf5ad
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-09-13 14:44:14 +0300

    update Header with links

--- thenovelclub/src/components/Header/Header.js
+++ thenovelclub/src/components/Header/Header.js
@@ -1,17 +1,17 @@
 import './Header.css';
-
+import { Link } from 'react-router-dom'
 
 export const Header = () => {
     return(
         <>
             <ul className='header-ul'>
-                <li className="header-parameter">Home</li>
-                <li className="header-parameter">Login</li>
-                <li className="header-parameter">Register</li>
-                <li className="header-parameter">Logout</li>
-                <li className="header-parameter">Catalog</li>
-                <li className="header-parameter">My Profile</li>
-                <li className="header-parameter">About</li>
+                <li className="header-parameter"><Link to='/'>Home</Link></li>
+                <li className="header-parameter"><Link to='/login'>Login</Link></li>
+                <li className="header-parameter"><Link to='/register'>Register</Link></li>
+                <li className="header-parameter"><Link to='/logout'>Logout</Link></li>
+                <li className="header-parameter"><Link to='/catalog'>Catalog</Link></li>
+                <li className="header-parameter"><Link to='/profile'>My Profile</Link></li>
+                <li className="header-parameter"><Link to='/about'>About</Link></li>
             </ul>
         </>
     )
