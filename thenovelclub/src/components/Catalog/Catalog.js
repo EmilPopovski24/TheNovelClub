66887b0d561ba8cb1d6ffcc68601b3f3839e2353
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-09-13 18:25:56 +0300

    Catalog creation

---
+++ thenovelclub/src/components/Catalog/Catalog.js
@@ -0,0 +1,19 @@
+import { BookItem } from "./BookItem/BookItem"
+
+export const Catalog = ({
+    books
+}) => {
+    return (
+        <>
+        {books.length > 0 && (
+            <h1 className="catalog-books">Books For Review</h1>
+        )}
+        <div className="catalog-page">
+            {books.map(x=> <BookItem key={x._id} {...x} />)}
+        </div>
+        {books.length === 0 & (
+            <h1 className="catalog-books">No books for review</h1>
+        )}
+        </>
+    )
+}
