3a61bf05606457351797533f59607c3c385d74b9
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-09-15 16:00:45 +0300

    Update authService.js

--- thenovelclub/src/services/authService.js
+++ thenovelclub/src/services/authService.js
@@ -1,18 +1,29 @@
-import * as request from './requester';
+import { requestFactory } from './requester';
 
-const baseUrl = "http://localhost:3030/users"
+const baseUrl = "http://localhost:3030/users";
 
-export const login = (loginData) => {
-    const result = request.post(`${baseUrl}/login`, loginData)
-    return result
-}
+export const authServiceFactory = (token) => {
 
-export const register = (registerData) => {
-    const result = request.post(`${baseUrl}/register`, registerData);
-    return result
-}
+    const request = requestFactory(token);
 
-export const logout = () => {
-    const result = request.get(`${baseUrl}/logout`);
-    return result;
+    const login = (loginData) => {
+        const result = request.post(`${baseUrl}/login`, loginData)
+        return result
+    }
+    
+     const register = (registerData) => {
+        const result = request.post(`${baseUrl}/register`, registerData);
+        return result
+    }
+    
+    const logout = () => {
+        const result = request.get(`${baseUrl}/logout`);
+        return result;
+    }
+
+    return {
+        login,
+        register,
+        logout
+    }
 }
