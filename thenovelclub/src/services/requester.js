13906ed9caefb877999562cd3b33455a0e414173
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-09-15 16:21:25 +0300

    Update requester.js

--- thenovelclub/src/services/requester.js
+++ thenovelclub/src/services/requester.js
@@ -1,50 +1,106 @@
-export const request = async (method, url, data, token) => {
+// export const request = async (method, url, data, token) => {
+//     const options = {};
+
+//     if (method !== 'GET') {
+//         options.method = method;
+
+//         if(data) {
+//             options.headers = {
+//                 'content-type': 'application/json',
+//             }
+//             options.body = JSON.stringify(data)
+//         }
+//     }
+
+//     if(token) {
+//         options.headers = {
+//             ...options.headers,
+//             'X-Authorization': token,
+//         };
+//     }
+
+//     const response = await fetch(url, options);
+
+//     if(response.status === 204) {
+//         return {}
+//     }
+
+//     const result = await response.json();
+
+//     if(!response.ok) {
+//         throw result
+//     }
+
+//     return result;
+
+// }
+
+
+
+// export const requestFactory = (token) => {
+
+//     return {
+//     get: request.bind(null, 'GET', token),
+//     post: request.bind(null, 'POST', token),
+//     put: request.bind(null, 'PUT', token),
+//     patch: request.bind(null, 'PATCH', token),
+//     delete: request.bind(null, 'DELETE', token)
+//     }
+    
+// }
+
+const request = async (method, token, url, data) => {
     const options = {};
-
-    if (method !== 'GET') {
-        options.method = method;
-
-        if(data) {
+    
+        if(method !== "GET") {
+            options.method = method;
+    
+            if(data) {
+                options.headers = {
+                    'content-type':'application/json', 
+                };
+                options.body = JSON.stringify(data);
+            }   
+        }
+    
+        if(token) {
             options.headers = {
-                'content-type': 'application/json',
-            }
-            options.body = JSON.stringify(data)
+                ...options.headers,
+                'X-Authorization': token,
+            };
+        }
+    
+        const response = await fetch(url, options);
+    
+        if(response.status === 204) {
+            return {};
+        }
+    
+        const result = await response.json();
+    
+        if(!response.ok) {
+            throw result;
+        }
+    
+        return result
+    };
+    
+    export const requestFactory = (token) => {
+        if(!token) {
+        const serializedAuth = localStorage.getItem('auth');
+
+        if(serializedAuth) {
+            const auth = JSON.parse(serializedAuth);
+            token = auth.accessToken
         }
     }
 
-    if(token) {
-        options.headers = {
-            ...options.headers,
-            'X-Authorization': token,
-        };
-    }
-
-    const response = await fetch(url, options);
-
-    if(response.status === 204) {
-        return {}
-    }
-
-    const result = await response.json();
-
-    if(!response.ok) {
-        throw result
-    }
-
-    return result;
-
-}
-
-
-
-export const requestFactory = (token) => {
-
-    return {
-    get: request.bind(null, 'GET', token),
-    post: request.bind(null, 'POST', token),
-    put: request.bind(null, 'PUT', token),
-    patch: request.bind(null, 'PATCH', token),
-    delete: request.bind(null, 'DELETE', token)
-    }
-    
-}
+        return {
+        get: request.bind(null, "GET", token),
+        post: request.bind(null, "POST", token),
+        put: request.bind(null, "PUT", token),
+        del: request.bind(null, "DELETE", token),
+        patch: request.bind(null, "PATCH", token),
+    };
+};
+   
