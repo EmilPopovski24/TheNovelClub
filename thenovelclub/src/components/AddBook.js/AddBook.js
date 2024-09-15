349010e713b00076ef2c5d32760b924ab6b9a53c
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-09-15 14:29:02 +0300

    Update AddBook.js

--- thenovelclub/src/components/AddBook.js/AddBook.js
+++ thenovelclub/src/components/AddBook.js/AddBook.js
@@ -1,27 +1,18 @@
-import { useState } from 'react';
+import { useForm } from '../../hooks/useForm';
 import './AddBook.css';
 
 export const AddBook = ({
     onAddBookSubmit
 }) => {
 
-    const [values, setValues] = useState({
+    const {values, changeHandler, onSubmit } = useForm({
         name: '',
         author: '',
         published: '',
         genre: '',
         coverUrl: '',
         description: '',
-    });
-
-    const onChangeHandler = (e) => {
-        setValues(state => ({...state, [e.target.name]: e.target.value}))
-    }
-
-    const onSubmit = (e) => {
-        e.preventDefault()
-        onAddBookSubmit(values);
-    }
+    }, onAddBookSubmit);
 
     return (
         <>
@@ -29,26 +20,27 @@ export const AddBook = ({
             <div className="shape"></div>
             <div className="shape"></div>
         </div>
+
         <h3>Add a Book</h3>
-        <form className="addBook-form" onSubmit={onSubmit}>
+        <form className="addBook-form" onSubmit={onSubmit} method="POST">
             
             <label htmlFor="name">Name</label>
-            <input type="text" placeholder="Book name..." id="name" value={values.name} onChange={onChangeHandler} name="name" required/>
+            <input type="text" placeholder="Book name..." id="name" value={values.name} onChange={changeHandler} name="name" required/>
 
             <label htmlFor="author">Author</label>
-            <input type="text" placeholder="Author..." id="author" value={values.author} onChange={onChangeHandler} name="author" required/>
+            <input type="text" placeholder="Author..." id="author" value={values.author} onChange={changeHandler} name="author" required/>
 
             <label htmlFor="published">Published on:</label>
-            <input type="text" placeholder="Published on..." id="published" value={values.published} onChange={onChangeHandler} name="published" required/>
+            <input type="text" placeholder="Published on..." id="published" value={values.published} onChange={changeHandler} name="published" required/>
 
             <label htmlFor="genre">Genre</label>
-            <input type="text" placeholder="Genre..." id="genre" value={values.genre} onChange={onChangeHandler} name="genre" required/>
+            <input type="text" placeholder="Genre..." id="genre" value={values.genre} onChange={changeHandler} name="genre" required/>
 
             <label htmlFor="coverUrl">Cover:</label>
-            <input type="text" placeholder="CoverURL..." id="coverUrl" value={values.coverUrl} onChange={onChangeHandler} name="coverUrl" required/>
+            <input type="text" placeholder="CoverURL..." id="coverUrl" value={values.coverUrl} onChange={changeHandler} name="coverUrl" required/>
 
             <label htmlFor="description">Description</label>
-            <textarea name="description" id="description" cols="40" rows="10" value={values.description} onChange={onChangeHandler} required></textarea>
+            <textarea name="description" id="description" cols="40" rows="10" value={values.description} onChange={changeHandler} required></textarea>
 
             <button>Add</button>
 
