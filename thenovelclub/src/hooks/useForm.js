a7f326cd844e921c7732564ae419d19a50f3e4f3
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-09-23 13:09:32 +0300

    Update useForm.js

--- thenovelclub/src/hooks/useForm.js
+++ thenovelclub/src/hooks/useForm.js
@@ -1,7 +1,7 @@
  import { useState } from "react";
 
-export const useForm = (initialValues, onSubmitHandler) => {
-    const [values, setValues] = useState(initialValues);
+export const useForm = (formValues, onSubmitHandler) => {
+    const [values, setValues] = useState(formValues);
 
     const changeHandler = (e) => {
         setValues(state => ({...state, [e.target.name]: e.target.value}))
