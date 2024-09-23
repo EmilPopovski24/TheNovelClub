08b1929260b5d056da0c435f0c901f2524d05fdc
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-09-23 13:32:50 +0300

    Update useForm.js

--- thenovelclub/src/hooks/useForm.js
+++ thenovelclub/src/hooks/useForm.js
@@ -13,10 +13,16 @@ export const useForm = (initialValues, onSubmitHandler) => {
         onSubmitHandler(values)
     }
 
+    const changeValues = (newValues) => {
+        //TODO: 
+        setValues(newValues)
+    }
+
     return {
         values, 
         changeHandler,
-        onSubmit 
+        onSubmit,
+        changeValues
     }
 };
 
