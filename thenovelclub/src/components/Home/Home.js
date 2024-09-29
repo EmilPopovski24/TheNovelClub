1085afd8dab5555010a4af44b8bd0fb4eb07ed7d
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-09-29 16:46:00 +0300

    Home page setup

--- thenovelclub/src/components/Home/Home.js
+++ thenovelclub/src/components/Home/Home.js
@@ -1,9 +1,9 @@
-
+import "./Home.css";
 
 export const Home = () => {
     return (
-        <p>
-            Home page
-        </p>
+        <>
+        <img src="https://www.economist.com/cdn-cgi/image/width=1424,quality=80,format=auto/content-assets/images/20240316_CUD001.jpg" alt="home-pic" className="home-pic"/>
+        </>
     )
 }
