d9d8afb439160f8af192b42c6b086b655b1c02c1
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-10-01 17:27:03 +0300

    Update commentService.js

--- thenovelclub/src/services/commentService.js
+++ thenovelclub/src/services/commentService.js
@@ -6,8 +6,8 @@ export const commentServiceFactory = (token) => {
 
     const request = requestFactory(token);
 
-    const create = async (commentData) => {
-        const comment = await request.post(baseUrl, commentData);
+    const create = async (bookId, commentData) => {
+        const comment = await request.post(baseUrl, bookId, commentData);
         return comment;
     }
     
