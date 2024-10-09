707da5ef1476136bbacfc7e8a83fd25fc59f2f19
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-10-09 10:06:50 +0300

    Update commentService.js

--- thenovelclub/src/services/commentService.js
+++ thenovelclub/src/services/commentService.js
@@ -12,6 +12,6 @@ export const getAll = async (bookId) => {
         const query = encodeURIComponent(`bookId="${bookId}"`);
         const author = encodeURIComponent(`author=_ownerId:users`);
         const result = await request.get(`${baseUrl}?where=${query}&load=${author}`);
-        const comments = Object.values(result);
-        return comments
+        // const comments = Object.values(result);
+        return result
 };
